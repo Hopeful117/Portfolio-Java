cd53c5b8420ba210fab708c88bb68d4d20cb5dde
ludo <ludovic.brot@gmail.com>
2026-08-26 19:49:10 +0200

    fix ci test failure

--- src/test/java/com/hopefull117/portfolio/java/JavaApplicationTests.java
+++ src/test/java/com/hopefull117/portfolio/java/JavaApplicationTests.java
@@ -2,9 +2,24 @@ package com.hopefull117.portfolio.java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class JavaApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0")
+			.withExposedPorts(27017);
+
+	@DynamicPropertySource
+	static void overrideProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
 	void contextLoads() {
